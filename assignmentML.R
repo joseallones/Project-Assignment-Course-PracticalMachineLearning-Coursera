b9d2f38d404263facaba3144b311786aa9defe1f
jose <joseallones87@gmail.com>
2015-08-24 11:12:32 +0200

    fix some problems

--- assignmentML.R
+++ assignmentML.R
@@ -21,10 +21,9 @@ testing <- dataset[-inTrain,]
 
 rf <- randomForest(training[,-53], training$classe,  ntree=300, do.trace=100)
 classesPrediction_randomForest <- predict(rf, newdata = testing[,-53])
-confusionMatrix(data = classesPrediction_randomForest, testing$classe)pwd
+confusionMatrix(data = classesPrediction_randomForest, testing$classe)
 
 
 rf_caret <- train(x=training[,-53],y=training$classe, method = "rf", ntree=300);
 classesPrediction_rf_caret <- predict(RFFit, newdata = testing[,-53])
 confusionMatrix(data = classesPrediction_rf_caret, testing$classe)
-
